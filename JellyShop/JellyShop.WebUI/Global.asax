5b27ff81ac4c4ebbe204c062395822500ab83730
Dipesh Patel <Dpatel216@dxc.com>
2020-08-02 16:36:07 -0500

    Initial Solution Setup

---
+++ JellyShop/JellyShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JellyShop.WebUI.MvcApplication" Language="C#" %>
---
+++ JellyShop/JellyShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JellyShop.WebUI.Startup))]
+namespace JellyShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
